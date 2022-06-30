dbaf7df660042d51d14304c1a8de7217eef51699
RxParalyze <c17phillip.smart@gmail.com>
2022-06-30 10:34:53 -0400

    Re-based to end of module 4

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,8 +1,9 @@
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpenses/NewExpense';
-import Card from './components/UI/Card';
+import React from 'react';
 
-function App() {
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -10,12 +11,7 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12)
-    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -33,19 +29,19 @@ function App() {
   const addExpenseHandler = expense => {
     console.log('In App.js');
     console.log(expense);
-  }
+  };
 
-  const addFilterHandler = filter => {
-    console.log('Added Filter');
-    console.log(filter);
-  }
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-        <Card className="expenses">
-          <Expenses expenses={expenses} onAddFilter={addFilterHandler} />
-        </Card>
+      <Expenses items={expenses} />
     </div>
   );
 }
--- react-guide/src/components/Expenses/Expenses.js
+++ react-guide/src/components/Expenses/Expenses.js
@@ -1,27 +1,44 @@
-import './Expenses.css';
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import { useState } from 'react';
+import './Expenses.css';
 
-function Expenses(props) {
-    let content = [];
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
 
-    const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
-    const saveFilterDataHandler = selectedYear => {
-        console.log('Expenses.js');
-        console.log(selectedYear);
-        setFilteredYear(selectedYear);
-    };
-
-    content.push(
-        <ExpensesFilter selected={filteredYear} onSaveFilterData={saveFilterDataHandler} />
-    );
-
-    for(let x in props.expenses) {
-        content.push(ExpenseItem(props.expenses[x]));
-    }
-    return content;
-}
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
